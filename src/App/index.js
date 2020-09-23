50aaa5243320326fee3bd73e85bfa928a18b8a22
CDBDX\nicolas.heulle <nicolas.heulle@ext.cdiscount.com>
2020-09-23 17:30:11 +0200

    first component react

---
+++ src/App/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Greetings from "../Greetings";
+
+export default function App() {
+  return (
+    <article>
+      <Greetings name="Nico" />
+      <Greetings name="toto" />
+    </article>
+  );
+}
